e2a76aa4db9cb5ba9430a9f4331ba8a20d035788
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-09-14 17:16:59 -0300

    populando seeder categories

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesSeeder extends Seeder
 {
@@ -13,5 +14,10 @@ class CategoriesSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('categories')->insert([
+            'name' => 'Electronics',
+            'slug' => 'electronics',
+            
+        ]);
     }
 }
