c362775ceb6eabe0a21d5f553bbfcb6272414e9d
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-09-14 16:41:58 -0300

    planejando rotas

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+/**
+ * - Rota de Utilidade
+ * [ ] - /ping - Responde com Pong
+ * ----------------------------------------------------------------
+ * - Rotas de Autenticação - Autenticação via Token
+ * ----------------------------------------------------------------
+ * [ ] - /user/signin - Login do Usuário
+ * [ ] - /user/signup - Registro do Usuário
+ * [ ] - /user/me - Informações do Usuário Logado
+ * ----------------------------------------------------------------
+ * - Rotas de Estado
+ * ----------------------------------------------------------------
+ * [ ] - /states - Listar os Estados
+ * [ ] - /categories - Listar as categorias do sistema
+ * ----------------------------------------------------------------
+ * - Rotas de Advertises
+ * [ ] - /ad/list - Listar todos os anúncios
+ * [ ] - /ad/:id - Dados de um anúncio único
+ * [ ] - /ad/add - Adicionar um novo anúncio
+ * [ ] - /ad/:id(PUT) - Alterar anúncio publicado
+ * [ ] - /ad/:id(delete) - Deletar um anúncio
+ * [ ] - /ad/:id/:image - Deletar imagem de um anúncio
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
