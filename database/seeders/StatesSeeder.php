fe3c58066f1579a5c8a0333a422c51a617fe78da
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-09-14 17:18:11 -0300

    populando seeder states

--- database/seeders/StatesSeeder.php
+++ database/seeders/StatesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class StatesSeeder extends Seeder
 {
@@ -13,5 +14,9 @@ class StatesSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('states')->insert([
+            'name' => 'Acre',
+            'slug' => 'acre',
+        ]);
     }
 }
