81e455ab0562ab850168ab2b519ceba398553680
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-09-15 13:04:21 -0300

    criando métodos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function signup(): JsonResponse
+    {
+        return response()->json(['method' => 'signup']);
+    }
+    public function signin(): JsonResponse
+    {
+        return response()->json(['method' => 'signin']);
+    }
+    public function me(): JsonResponse
+    {
+        return response()->json(['method' => 'me']);
+    }
 }
